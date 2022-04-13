04e0a3ce3c69845dcd3bcd24641a43d0d2cae951
ctznfive <ctznfive@gmail.com>
2022-04-13 23:14:59 +0300

    Add profiles

--- js-file.js
+++ js-file.js
@@ -8,10 +8,35 @@ function getValue() {
     let eRes = document.getElementById('eRes');
     let eoRes = document.getElementById('eoRes');
     let eopRes = document.getElementById('eopRes');
-    eRes.textContent = 'B, kg : ' + eNum;
-    eoRes.textContent = 'B + H, kg : ' + eo.toFixed(2);
-    eopRes.textContent = 'B + H + T, kg : ' + eop.toFixed(2);
+    eRes.textContent = 'B, kg \xa0\xa0\xa0\xa0-\xa0 ' + eNum.toFixed(2);
+    eoRes.textContent = 'B+H, kg \xa0\xa0-\xa0 ' + eo.toFixed(2);
+    eopRes.textContent = 'B+H+T, kg -\xa0 ' + eop.toFixed(2);
+}
+
+function changeProfile(a, b) {
+    document.getElementById('oNum').value = parseFloat(a);
+    document.getElementById('pNum').value = parseFloat(b);
 }
 
 let calc = document.getElementById('calc');
 calc.addEventListener('click', getValue, false); 
+
+document.getElementById('profile1').addEventListener('click',() => {
+    document.getElementById('oNum').value = 50;
+    document.getElementById('pNum').value = 13;
+})
+
+document.getElementById('profile2').addEventListener('click',() => {
+    document.getElementById('oNum').value = 45;
+    document.getElementById('pNum').value = 13;
+})
+
+document.getElementById('profile3').addEventListener('click',() => {
+    document.getElementById('oNum').value = 40;
+    document.getElementById('pNum').value = 13;
+})
+
+document.getElementById('profile4').addEventListener('click',() => {
+    document.getElementById('oNum').value = 30;
+    document.getElementById('pNum').value = 10;
+})
